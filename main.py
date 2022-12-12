fd4de45af9707eede213820c81ea9293c6d2613c
Mahmood Ramzani <mahmoodramzani@gmail.com>
2022-12-12 08:35:13 +0330

    change source of ips to ripe

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tld import get_tld
 
-domains = set()
-ips = set()
+nekoray_domains = set()
+nekoray_ips = set()
 
 
 def read_domains(file_name):
@@ -29,7 +29,7 @@ def read_domains(file_name):
         res = get_tld(d, as_object=True, fix_protocol=True, fail_silently=True)
         if res is not None:
             out = "domain:{0}\n".format(res.fld)
-            domains.add(out)
+            nekoray_domains.add(out)
 
     file.close()
 
@@ -53,22 +53,21 @@ def read_ips(file_name):
         d = line.strip()
         print(d)
         out = "{0}\n".format(d)
-        ips.add(out)
+        nekoray_ips.add(out)
 
     file.close()
 
 
-read_domains("domains.txt")
 read_domains("ito.txt")
-read_domains("my-domains.txt")
+# read_domains("my-domains.txt")
 
-file_output = open('iran_domains.txt', 'w')
-file_output.writelines(domains)
+file_output = open('nekoray_domains.txt', 'w')
+file_output.writelines(nekoray_domains)
 file_output.close()
 
-read_ips("firewall.txt")
-read_ips("my-ips.txt")
+read_ips("ripe.txt")
+# read_ips("my-ips.txt")
 
-file_output = open('iran_ips.txt', 'w')
-file_output.writelines(ips)
+file_output = open('nekoray_ips.txt', 'w')
+file_output.writelines(nekoray_ips)
 file_output.close()
